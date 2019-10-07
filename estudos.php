e82f8f5b4d67ae2aadc51fd5a9ceca12f2e5cc17
Thiagolivramento <54681189+Thiagolivramento@users.noreply.github.com>
2019-10-07 20:09:16 -0300

    Add files via upload
    Apenas estudos

---
+++ estudos.php
@@ -0,0 +1,15 @@
+<?php
+
+  echo "Ola, essa frase foi escrita através da função echo<br>";
+
+  echo "essa é outra linha.";
+
+  //fazendo comentário no php
+  /*
+  essa também é uma forma de fazer comentário
+
+  */
+  
+
+  print("<br>Essa frase é imprensa com comando print");
+?>
