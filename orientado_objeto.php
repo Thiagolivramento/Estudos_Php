72203a196ec61e7dd82d4b12b342b80a7b572e33
Thiagolivramento <tlivramento.adapta@gmail.com>
2019-10-30 15:11:14 -0300

    novos estudos de php

---
+++ orientado_objeto.php
@@ -0,0 +1,36 @@
+<?php
+
+/*pequeno estudo sobre orientação ao objeto em php
+* contemplando algumas características de como realizar.
+* dentro do php existem as classes e os objetos
+ex: classe - pessoa... objeto - thiago
+dentro das classes existem os campos e os metodos
+campos são as variaveis e o metodos as funções.
+*/
+
+//criando uma classe
+
+class Pessoa{
+    public $nome; //public informa que a variavel é publica.
+    public $idade;
+//função falar nome
+    public function falarNome() {
+        echo $this->nome;
+    }
+    public function falarIdade(){
+        echo $this->idade;
+    }
+}
+
+$uma_pessoa = new Pessoa; //new significa que estou criando um objeto para a classe pessoa.
+$uma_pessoa->nome = "Thiago";/* aqui o objeto  está recebendo a string thiago para ser alocada
+na variavel nome da classe pessoa. O mesmo ocorre na idade.
+*/
+$uma_pessoa->idade = 36;
+
+var_dump($uma_pessoa); //estou vendo qual é o tipo e tamanho da variavel uma pessoa.
+
+// para chamar a função que está dentro da classe, utilizar exemplo abaixo. Lembrar de excluir o var_dump.
+
+$uma_pessoa-> falarNome();
+$uma_pessoa-> falarIdade();//chamando a função falar idade.
