8bb6e3f54022381706094d57d6552512cf29f873
Thiagolivramento <tlivramento.adapta@gmail.com>
2019-11-04 13:57:12 -0300

    atualizando

--- if_else.php
+++ if_else.php
@@ -9,4 +9,14 @@
       
      //aqui fica o que ele executa, caso verdadeira a condição.
  
+    
+    }else if (condição){
+
+        //aqui fica a o que executa, caso o if nao seja verdadeiro.
+    
+    
+     else {
+
+     //aqui fica o que executa, caso o if e o else if não seja verdadeiro. Só pode ser usado uma vez.
+
     }
