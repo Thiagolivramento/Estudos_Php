02b89c577c0843a0ce7593cb72187f66ee295a36
Thiagolivramento <tlivramento.adapta@gmail.com>
2019-11-04 15:22:53 -0300

    estudos de php

---
+++ for.php
@@ -0,0 +1,9 @@
+<?php
+
+//comando for, usado como contador no php. Tem inicio e fim. dentro do paretese vai a condição do for.
+
+for ($i = 0; $i < 10; $i++){
+
+    echo $i . "<br>";
+
+   }
---
+++ switch_case.php
@@ -0,0 +1,27 @@
+<?php
+
+$diaDaSemana = date("w");
+
+//vou colocar um switch e a condição dele, pode ser o diaDaSemana.
+
+switch (condição){
+    case 0:
+    echo "domingo";
+    break;
+
+    case 1:
+    echo "segunda";
+    break;
+
+    //e assim vai colocando os casos. O break serve para parar o switch quando for a condição.
+   
+   //caso precise, utiliza o default que serve quando nenhum caso é realizado.
+   default:
+   echo "data inválida";
+   break;
+
+   }
+
+
+          
+         
